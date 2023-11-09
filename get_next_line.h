78714d11e974e65b8cf071401e7fb09a513ad2ab
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-11-09 08:53:12 +0100

    Edit get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,15 @@
 /*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 08:50:13 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/11/09 08:50:13 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/11/09 08:52:56 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
