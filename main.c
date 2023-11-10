e535b908d9c34de18dbd489db30d39e852eaa71a
Julien Houyet <julien.houyet@teradev.be>
2023-11-10 18:56:42 +0100

    Update for norminette

--- main.c
+++ main.c
@@ -6,11 +6,9 @@ int main(void)
   int  	fd;
   char  *line;
   int  	count;
-  int	test;
 
   count = 0;
-  test = 0;
-  fd = open("./files/43_with_nl", O_RDONLY);
+  fd = open("./files/errors.txt", O_RDONLY);
   if (fd == -1)
   {
 	printf("Error with file.");
@@ -25,7 +23,6 @@ int main(void)
 	printf("[%d]:%s\n", count, line);
 	free(line);
 	line = NULL;
-	test++;
   }
   close(fd);
   system("leaks a.out");
